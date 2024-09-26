658100140e77293f0d1bfd7bec323e631980d28e
kubaz <zurawikjakub@gmail.com>
2024-09-26 16:31:32 +0200

    Adding EnhancedInput Module To Build

--- Source/Overforged/Public/PlayerCharacter.h
+++ Source/Overforged/Public/PlayerCharacter.h
@@ -1,10 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class OVERFORGED_API APlayerCharacter : public ACharacter
@@ -12,23 +13,24 @@ class OVERFORGED_API APlayerCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+	
 	APlayerCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* InputMappingContext;
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* InputAction;
+
+
 private:
+	
 
-	UPROPERTY(EditAnywhere)
-	UStaticMeshComponent* PlayerMesh;
-
+	
 };
